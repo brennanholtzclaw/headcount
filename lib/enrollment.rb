43ff15fa2e3cd32a209fc65fcfff479126bf0cc9
Beth Sebian <esebian@gmail.com>
2015-11-11 13:23:26 -0700

    add kindergarten participation by year and in year methods to enrollment object

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,3 +1,5 @@
+require_relative 'enrollment_repository'
+
 class Enrollment
   attr_reader :data
 
@@ -6,13 +8,14 @@ class Enrollment
   end
 
   def kindergarten_participation_by_year
-    ## returns hash with percentage for all years in data
+    data[:kindergarten_participation]
   end
 
   def kindergarten_participation_in_year(year)
-    ##returns nil if given unknown year
-    ##returns floatnum for percentage
+      data[:kindergarten_participation][year]
   end
-  
 end
 
+# <Enrollment:0xXXXXXX @data={:name=>"COLORADO", :kindergarten_participation=>{"2007"=>"0.39465", "2006"=>"0.33677", "2005"=>"0.27807", "2004"=>"0.24014", "2008"=>"0.5357", "2009"=>"0.598", "2010"=>"0.64019", "2011"=>"0.672", "2012"=>"0.695", "2013"=>"0.70263", "2014"=>"0.74118"}}>
+
+#TODO Return 3-digit floatnum instead of string
--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -6,13 +6,42 @@ require_relative '../lib/enrollment'
 
 class EnrollmentTest < Minitest::Test
 
-  def test_returns_percentage_for_all_years_in_data
+  def test_it_is_initialized_with_data
+    data = {:name=>"COLORADO",
+            :kindergarten_participation=>{"2007"=>"0.39465", "2006"=>"0.33677", "2005"=>"0.27807", "2004"=>"0.24014", "2008"=>"0.5357", "2009"=>"0.598", "2010"=>"0.64019", "2011"=>"0.672", "2012"=>"0.695", "2013"=>"0.70263", "2014"=>"0.74118"}}
+    e = Enrollment.new(data)
 
+    assert_equal data, e.data
   end
 
   def test_returns_kindergarten_participation_by_year
-    ##returns nil if given unknown year
-    ##returns floatnum for percentage
+    data = {:name=>"COLORADO",
+            :kindergarten_participation=>{"2007"=>"0.39465", "2006"=>"0.33677", "2005"=>"0.27807", "2004"=>"0.24014", "2008"=>"0.5357", "2009"=>"0.598", "2010"=>"0.64019", "2011"=>"0.672", "2012"=>"0.695", "2013"=>"0.70263", "2014"=>"0.74118"}}
+    e = Enrollment.new(data)
+
+    assert_equal data[:kindergarten_participation], e.kindergarten_participation_by_year
   end
+
+  def test_returns_kindergarten_participation_in_year
+    data = {:name=>"COLORADO",
+            :kindergarten_participation=>{"2007"=>"0.39465", "2006"=>"0.33677", "2005"=>"0.27807", "2004"=>"0.24014", "2008"=>"0.5357", "2009"=>"0.598", "2010"=>"0.64019", "2011"=>"0.672", "2012"=>"0.695", "2013"=>"0.70263", "2014"=>"0.74118"}}
+    e = Enrollment.new(data)
+
+    assert_equal "0.24014", e.kindergarten_participation_in_year("2004")
+  end
+
+  def test_returns_nil_if_no_data_included
+    data = {:name=>"COLORADO",
+            :kindergarten_participation=>{"2007"=>"0.39465", "2006"=>"0.33677", "2005"=>"0.27807", "2004"=>"0.24014", "2008"=>"0.5357", "2009"=>"0.598", "2010"=>"0.64019", "2011"=>"0.672", "2012"=>"0.695", "2013"=>"0.70263", "2014"=>"0.74118"}}
+    e = Enrollment.new(data)
+
+    assert_nil e.kindergarten_participation_in_year("1998")
+  end
+
+
+
+  ##returns nil if given unknown year
+  ##returns floatnum for percentage
+
 
 end
