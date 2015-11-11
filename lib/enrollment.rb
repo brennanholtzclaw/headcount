89549b3dc25fd4f5a0da88bfdef4e99a938ce6d5
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-11-10 20:44:01 -0700

    clearing out files I shouldn't have touched

--- lib/enrollment.rb
+++
@@ -1,6 +0,0 @@
-#def kindergarten_participation_by_year
-## returns hash with percentage for all years in data
-
-#def kindergarten_participation_in_year(year)
-##returns nil if given unknown year
-##returns floatnum for percentage
