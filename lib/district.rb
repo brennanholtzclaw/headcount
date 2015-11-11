578d02c244bdf2e97f78355b86c35521f39d574d
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-11-10 20:07:48 -0700

    small flesh out of data points for district class

--- lib/district.rb
+++ lib/district.rb
@@ -15,7 +15,19 @@ class District
   end
 
   def name
-    @district[:district]
+    @district[:district].upcase
+  end
+
+  def years
+    @district[:years]
+  end
+
+  def data_format
+    @district[:data_format]
+  end
+
+  def data
+    @district[:data]
   end
 
 end
