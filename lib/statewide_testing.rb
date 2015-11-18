55bba6f56930ef0dd7201d0171f073781d1782c4
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-11-18 13:41:16 -0700

    pulling out errors into their own methods to try and shrink things down a bit

--- lib/statewide_testing.rb
+++ lib/statewide_testing.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class StatewideTest
   attr_reader :data
+  attr_accessor :race, :subject, :grade, :year
 
   def initialize(data)
     @data = data
@@ -27,26 +28,62 @@ class StatewideTest
     years.flatten.uniq
   end
 
-  def race_file_scores(race, year)
-    races = { :asian => "Asian",
-              :black => "Black",
-              :pacific_islander => "Hawaiian/Pacific Islander",
-              :hispanic => "Hispanic",
-              :native_american => "Native American",
-              :two_or_more => "Two or More",
-              :white => "White"}
+  def is_a_valid_year?(year)
+    year_files = [:third_grade, :eighth_grade]
+    years = year_files.map do |file_symbol|
+      @data[file_symbol].keys
+    end
+    years.flatten.uniq
+    if years.flatten.uniq.include?(year)
+      @year = year
+    else
+      raise "UnknownDataError"
+    end
+  end
 
+  def is_a_valid_race?(race)
+    races = { :asian => "Asian", :black => "Black",
+              :pacific_islander => "Hawaiian/Pacific Islander", :hispanic => "Hispanic",
+              :native_american => "Native American", :two_or_more => "Two or More",
+              :white => "White"}
     if races[race]
-      race = races[race]
+      @race = races[race]
+      true
     else
       raise "UnknownRaceError"
     end
+  end
+
+  def is_a_valid_subject?(subject)
+    subjects = { :math => "Math",
+              :reading => "Reading",
+              :writing => "Writing"}
+    if subjects[subject]
+      @subject = subjects[subject]
+      true
+    else
+      raise "UnknownDataError"
+    end
+  end
+
+  def is_a_valid_grade?(grade)
+    grades = {3 => :third_grade, 8 => :eighth_grade}
+    if grades[grade]
+      @grade = grades[grade]
+      true
+    else
+      raise "UnknownDataError"
+    end
+  end
+
+  def race_file_scores(race, year)
+    is_a_valid_race?(race)
 
     scores = {}
     race_files = [:math, :reading, :writing]
 
     race_files.each do |symbol|
-      scores[symbol] = @data[symbol][race][year]
+      scores[symbol] = @data[symbol][@race][year]
     end
     scores
   end
@@ -54,15 +91,7 @@ class StatewideTest
   def proficient_by_race_or_ethnicity(race)
     years = all_race_file_years
     race_data = {}
-    races = { :asian => "Asian",
-              :black => "Black",
-              :pacific_islander => "Hawaiian/Pacific Islander",
-              :hispanic => "Hispanic",
-              :native_american => "Native American",
-              :two_or_more => "Two or More",
-              :white => "White"}
-
-    raise "UnknownRaceError" if races[race].nil?
+    is_a_valid_race?(race)
 
     years.each do |year|
       race_data[year] = race_file_scores(race, year)
@@ -71,31 +100,17 @@ class StatewideTest
   end
 
   def proficient_for_subject_by_grade_in_year(subject, grade, year)
-    grades = {3 => :third_grade, 8 => :eighth_grade}
-    subjects = { :math => "Math",
-              :reading => "Reading",
-              :writing => "Writing"}
+    is_a_valid_subject?(subject)
+    is_a_valid_grade?(grade)
+    is_a_valid_year?(year)
 
-    raise "UnknownDataError" if subjects[subject].nil? || grades[grade].nil? | !data[grades[grade]].keys.include?(year)
-
-    @data[grades[grade]][year][subjects[subject]]
-
+    @data[@grade][year][@subject]
   end
 
   def proficient_for_subject_by_race_in_year(subject, race, year)
-    races = { :asian => "Asian",
-              :black => "Black",
-              :pacific_islander => "Hawaiian/Pacific Islander",
-              :hispanic => "Hispanic",
-              :native_american => "Native American",
-              :two_or_more => "Two or More",
-              :white => "White"}
-    subjects = { :math => "Math",
-              :reading => "Reading",
-              :writing => "Writing"}
-
-    raise "UnknownDataError" if subjects[subject].nil? || races[race].nil? ||  @data[subject][races[race]].keys.include?(2016)
-    
-    @data[subject][races[race]][year]
+    is_a_valid_race?(race)
+    is_a_valid_year?(year)
+    is_a_valid_subject?(subject)
+    @data[subject][@race][@year]
   end
 end
